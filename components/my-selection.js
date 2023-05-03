ab40bfeb2e1b55d2269bb731db5ddd4c4f5aef52
havertzz04 <havertzz04@gmail.com>
2023-05-03 13:46:16 -0500

    first commit

---
+++ components/my-selection.js
@@ -0,0 +1,19 @@
+let pathName = new URL(import.meta.url).pathname;
+let name = pathName.split('/').pop().replace(".js", "");
+
+export default class mySelection extends HTMLElement {
+
+    static async components() {
+        return await (await fetch(pathName.replace(".js", ".html"))).text();
+    }
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        Promise.resolve(mySelection.components()).then(html => {
+            this.shadowRoot.innerHTML = html;
+        })
+        console.log("mySelection funcionando")
+    }
+}
+
+customElements.define(name, mySelection);
